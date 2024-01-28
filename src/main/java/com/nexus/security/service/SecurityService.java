d7b381fae55221cd09e026a9eb6899ea709ab92e
igorcampos-dev <icampos.developer@yahoo.com>
2024-01-28 02:38:34 -0300

    FEAT: inicio da implementacao do spring security para gerenciar rotas

---
+++ src/main/java/com/nexus/security/service/SecurityService.java
@@ -0,0 +1,17 @@
+package com.nexus.security.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@AllArgsConstructor
+public class SecurityService {
+
+    private final HttpSecurity httpSecurity;
+
+    public static SecurityFilterChain configureSecurity(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.build();
+    }
+}
