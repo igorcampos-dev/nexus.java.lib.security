939f029ee86e546d8491b824672073b43b367fd2
igorcampos-dev <icampos.developer@yahoo.com>
2024-01-29 22:15:21 -0300

    FIX: correcao na implementacao de notacao

--- src/main/java/com/nexus/security/service/JwtService.java
+++ src/main/java/com/nexus/security/service/JwtService.java
@@ -2,7 +2,9 @@ package com.nexus.security.service;
 
 import com.nexus.security.model.dto.TokenPropertiesDTO;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface JwtService {
     String encode(UserDetails userDetails);
     TokenPropertiesDTO decode(String token);
